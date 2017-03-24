975afc0e3d1650dbf9d2c1f5633d70af57a24120
Gil Shomron <gilsho@eed.ef.technion.ac.il>
2017-03-24 18:20:54 +0300

    Added text to results section

--- _paper/scripts/exec_time.py
+++ _paper/scripts/exec_time.py
@@ -7,8 +7,8 @@ import pylab as pylab
 n_groups = 3
 
 workloads = ('CPU', 'GPU w/o cuDNN', 'GPU w cuDNN')
-ssd = (1, 2, 3)
-yolo = (1.5, 2.5, 3.5)
+ssd = (3000, 259.013, 201.41)
+yolo = (20000, 186.364, 204.918)
 
 fig = plt.figure(figsize=(10,6))
 ax = fig.add_subplot(111)
@@ -18,13 +18,13 @@ bar_width = 0.3
 rects1 = plt.bar(index, ssd, bar_width, zorder=4, color='0.8', label="SSD")
 rects2 = plt.bar(index+bar_width, yolo, bar_width, zorder=4, color='0.6', label="YOLO") 
 
-plt.ylabel('Time [sec]',fontsize=16)
+plt.ylabel('Time [msec]',fontsize=16)
 
 ax.yaxis.grid(linestyle='-', zorder=0, alpha=0.5)
 plt.tick_params(axis='x', bottom='off', top='off')
-plt.legend(loc=2)
+plt.legend(loc=1)
 ax.set_xlim([-0.2, 2.82])
-ax.set_ylim([0, 4])
+ax.set_ylim([0, 300])
 
 plt.subplots_adjust(top=0.26)
 
